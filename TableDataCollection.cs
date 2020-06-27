2ca3710e1f586aba3021e322082d500ce185bb7d
Lena Mavricheva <59619343+mavrichevaWS@users.noreply.github.com>
2020-06-27 03:59:39 +0300

    Create TableDataCollection.cs

---
+++ TableDataCollection.cs
@@ -0,0 +1,13 @@
+ namespace SeleniumJenkinsTests
+{
+    public class TableDataColection
+    {
+        public string ColumnName { get; set; }
+        public string ColumnValue { get; set; }
+    }
+
+    public class TableDataColection2
+    {
+        public object Row { get; set; }
+    }
+}
